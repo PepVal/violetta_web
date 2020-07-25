9a29684be45ae98aaa245359176cf5839b707b3f
PepVal <pepval001@gmail.com>
2020-07-25 01:26:26 -0500

    custom navbar

--- components/Container.js
+++ components/Container.js
@@ -13,7 +13,7 @@ const Container = (props) => {
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
             </Head>
             <Navigation />
-            <div>
+            <div style={{marginTop: "92px"}}>
                 {props.children}
             </div>
         </div>
--- components/navigation.js
+++ components/navigation.js
@@ -1,44 +1,85 @@
 import Link from 'next/link'
 import Router from 'next/router'
 
-const Navigation = () => {
+class Navigation extends React.Component {
 
-    return (
-        <ul className="row align-items-center justify-content-around navBar">
-            <div>
-                <h2>
-                    <Link href="/">
-                        Violetta
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: -1,
+        }
+    }
+
+    handleNav = (op) => {
+        console.log(op)
+        switch (op) {
+            case 0:
+                Router.push('/categories/[id]', '/categories/hombre')
+                this.setState({ active: 0 })
+                break;
+            case 1:
+                Router.push('/categories/[id]', '/categories/mujer')
+                this.setState({ active: 1 })
+                break;
+            case 2:
+                Router.push('/categories/[id]', '/categories/niños')
+                this.setState({ active: 2 })
+                break;
+            case 3:
+                Router.push('/categories/[id]', '/categories/accesorios')
+                this.setState({ active: 3 })
+                break;
+            default:
+                this.setState({ active: -1 })
+        }
+    }
+
+    search(e){
+        e.preventDefault()
+    }
+
+    render() {
+        const { active } = this.state;
+        return (
+            <header className="navBar align-items-center">
+                <div></div>
+                <div>
+                    <h2 className="title">
+                        <Link href="/">
+                            <a>Violetta</a>
+                        </Link>
+                    </h2>
+                </div>
+                <ul className="items-group">
+                    <li onClick={(e) => this.handleNav(0)}>
+                        <a className={active == 0 ? "active" : ""}>HOMBRE</a>
+                    </li>
+                    <li onClick={(e) => this.handleNav(1)}>
+                        <a className={active == 1 ? "active" : ""}>MUJER</a>
+                    </li>
+                    <li onClick={(e) => this.handleNav(2)}>
+                        <a className={active == 2 ? "active" : ""}>NIÑOS</a>
+                    </li>
+                    <li onClick={(e) => this.handleNav(3)}>
+                        <a className={active == 3 ? "active" : ""}>ACCESORIOS</a>
+                    </li>
+                </ul>
+                <div className="input-search">
+                    <form onSubmit={(e) => this.search(e)}>
+                        <input placeholder="Buscar" />
+                    </form>
+                </div>
+                <div className="nav-icons">
+                    <Link href="/cart">
+                        <img src="/cart.svg" alt="Carrito" />
                     </Link>
-                </h2>
-            </div>
-            <div className="row justify-content-around">
-                <li onClick={(e) => Router.push('/categories/[id]', '/categories/hombre') }>
-                    Hombre
-                </li>
-                <li onClick={(e) => Router.push('/categories/[id]', '/categories/mujer') }>
-                    Mujer
-                </li>
-                <li onClick={(e) => Router.push('/categories/[id]', '/categories/niños') }>                    
-                    Niños
-                </li>
-                <li onClick={(e) => Router.push('/categories/[id]', '/categories/accesorios') }>
-                    Accesorios
-                </li>
-            </div>
-            <div>
-                <input/>
-            </div>
-            <div>
-                <Link href="/cart">
-                    <img src="/cart.svg" alt="Carrito" />
-                </Link>
-                <Link href="/login">
-                    <img src="/user.svg" alt="Perfil" />
-                </Link>
-            </div>
-        </ul>
-    )
+                    <Link href="/login">
+                        <img src="/user.svg" alt="Perfil" />
+                    </Link>
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Navigation

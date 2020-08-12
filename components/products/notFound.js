3c619e1d27d547f5fe2b8f2af6ca0e09a885e147
PepVal <pepval001@gmail.com>
2020-08-12 00:58:44 -0500

    fix some bugs and add favicon to the head

--- components/products/notFound.js
+++ components/products/notFound.js
@@ -4,7 +4,7 @@ const NotFoundProduct = () => {
         <div className="not-found">
             <h2>Parece que no podemos encontrar la página que estás buscando.</h2>
 
-            <h3>Se ha producido un error 404.</h3>
+            <h3>Se ha producido un error (404).</h3>
             <p>Haz click en el botón de atrás de tu navegador o mira estos productos relacionados</p>
         </div>
     )
--- pages/categories/[id].js
+++ pages/categories/[id].js
@@ -1,39 +1,53 @@
 import Head from 'next/head'
-import fetch from 'isomorphic-fetch'
 import { useRouter } from 'next/router'
-import ProductC from '../../components/categories/cardproduct'
+
+import fetch from 'isomorphic-fetch'
+
 import Container from '../../components/Container'
 import Footer from '../../components/footer'
+
+import ProductC from '../../components/categories/cardproduct'
 import FilterC from '../../components/categories/collapseCategories'
 import Error404 from '../../components/categories/Error404'
 
 const Categories = (props) => {
     const router = useRouter();
-    const {id} = router.query;
+    const { id } = router.query;
+
+    try {
+        // sube el scroll automaticamente :D
+        document.documentElement.scrollTop = 0;
+    } catch (error) {
+        console.log(error)
+    }
 
     return (
         <Container category={id}>
+            <Head>
+                <title>Categoria: {id}</title>
+            </Head>
+
             <div className="product-container">
                 {/*<h1>Soy Categories {id}</h1>*/}
                 <FilterC />
                 <ul className="custom-row justify-content-center">
-                    {   id ==="hombre" ? 
-                        props.category.hombre.map((product,id)   => (<ProductC key={id} data={product}></ProductC>))
+                    {id === "hombre" ?
+                        props.category.hombre.map((product, id) => (<ProductC key={id} data={product} />))
                         :
-                        id ==="mujer"?
-                        props.category.mujer.map((product,id)   => (<ProductC key={id} data={product}></ProductC>))
-                        :
-                        id ==="niños"?
-                        props.category.niños.map((product,id)   => (<ProductC key={id} data={product}></ProductC>))
-                        :
-                        id ==="accesorios"?
-                        props.category.accesorios.map((product,id)   => (<ProductC key={id} data={product}></ProductC>))
-                        :
-                        <Error404></Error404>
+                        id === "mujer" ?
+                            props.category.mujer.map((product, id) => (<ProductC key={id} data={product} />))
+                            :
+                            id === "niños" ?
+                                props.category.niños.map((product, id) => (<ProductC key={id} data={product} />))
+                                :
+                                id === "accesorios" ?
+                                    props.category.accesorios.map((product, id) => (<ProductC key={id} data={product} />))
+                                    :
+                                    <Error404 />
                     }
                 </ul>
-                <Footer/>
             </div>
+            <Footer />
         </Container>
     )
 }
@@ -41,7 +55,7 @@ Categories.getInitialProps = async (ctx) => {
     const res = await fetch('https://my-json-server.typicode.com/PepVal/fake-server/category')
     const data = await res.json()
 
-    return {category: data}
+    return { category: data }
 }
 
 
